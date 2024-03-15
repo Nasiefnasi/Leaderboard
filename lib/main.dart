0110886e70ec8e45182beaf1ec9c9f03e2a1b5f6
muhammad nasief <nasinasief@gmail.com>
2024-03-15 11:24:03 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:leaderboardapp/view/leadership/leadership.dart';
+
+void main() {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home:  LeaderBoard(),
+    );
+  }
+}
+
---
+++ lib/view/leadership/leadership.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LeaderBoard extends StatelessWidget {
+  const LeaderBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: const Text("Leaderboard"),),
+      body: const SafeArea(
+          child: Column(
+        children: [],
+      )),
+    );
+  }
+}
